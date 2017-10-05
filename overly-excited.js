4ae0c0f7086e068d3eb982a33c43c43c02442298
Tyler Bowman <tylerbowman@Tylers-MacBook-Pro.local>
2017-10-05 11:57:02 -0500

    refactored overlyExcited function

--- overly-excited.js
+++ overly-excited.js
@@ -1,14 +1,24 @@
 let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon" ];
 
+let betterSentence = ["Hello", "my", "name", "is", "Tyler", "and", "I", "am", "in", "NSS", "cohort", "22"];
 let addExcitement = (theWordArray, punctuation) => {
-    for (var index = 0; index <= theWordArray.length; index++) {
-        
-        if (index % 3 === 0 && index != 0) { 
-            theWordArray[index-1] += punctuation.repeat(index/3);
-        }
-        console.log(theWordArray.slice(0, index).join(" ")); 
+    let finalOutput = ""
+    for (var index = 0; index < theWordArray.length; index++) {
+        const position = index + 1;
+
+        if (position % 3 === 0) {
+            finalOutput = finalOutput + " " + theWordArray[index] + punctuation.repeat(position / 3); 
+        } else {
+            finalOutput = finalOutput + " " + theWordArray[index];
+        }; 
+        document.write(finalOutput);
+        console.log(finalOutput)
     }
 }
+
+addExcitement(sentence, "?");
+
+addExcitement(betterSentence, "!" )
 
 
     
